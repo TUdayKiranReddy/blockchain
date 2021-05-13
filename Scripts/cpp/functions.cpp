30ee22b87d7729d758a4bbce399fb9ecbff27af8
TUdayKiranReddy <53467166+TUdayKiranReddy@users.noreply.github.com>
2021-05-14 00:53:20 +0530

    Add files via upload

--- Scripts/cpp/functions.cpp
+++ Scripts/cpp/functions.cpp
@@ -663,7 +663,136 @@ T* create_all_membership_witnesses(T A0, HashMapTable S, T n){
 	return root_factor(A0, primes, S.length, n, rfactor_len);
 }
 
+cpp_int* prove_non_membership(cpp_int A0, HashMapTable S, cpp_int x, int x_nonce, cpp_int n){
+	if(S.SearchKey(x)!=-1)
+		return NULL;
+	else{
+		cpp_int* result = new cpp_int[2];
+		cpp_int product = 1;
+		cpp_int* key = S.keys();
+		cpp_int element;
+		int nonce;
+		for(int i=0;i<S.length;i++){
+			element = key[i];
+			nonce = S.SearchKey(element);
+			product *= hash_to_prime(element, ACCUMULATED_PRIME_SIZE, nonce)[0];
+		}
+		cpp_int prime = hash_to_prime(x, ACCUMULATED_PRIME_SIZE, x_nonce)[0];
+		cpp_int* a_b = bezout_coefficients<cpp_int>(prime, product);
+		if(a_b[0] < 0){
+			cpp_int positive_a = -1*a_b[0];
+			cpp_int inverse_A0 = mul_inv<cpp_int>(A0, n);
+			result[0] = power<cpp_int>(inverse_A0, positive_a, n);
+		}
+		else{
+			result[0] = power<cpp_int>(A0, a_b[0], n);
+		}
+		result[1] = a_b[1];
+		return result;
+	}
+}
+		
+bool verify_non_membership(cpp_int A0, cpp_int A_final, cpp_int d, cpp_int b, cpp_int x, int x_nonce, cpp_int n){
+	cpp_int prime = hash_to_prime(x, ACCUMULATED_PRIME_SIZE, x_nonce)[0];
+	cpp_int second_power;
+	if(b < 0){
+		cpp_int positive_b = -1*b;
+		cpp_int inverse_A_final = mul_inv<cpp_int>(A_final, n);
+		second_power = power<cpp_int>(inverse_A_final, positive_b, n);
+	}
+	else{
+		second_power = power<cpp_int>(A_final, b, n);
+	}
+	return ((power<cpp_int>(d, prime, n) * second_power) % n) == A0;
+}
+	
+cpp_int* aggregate_membership_witnesses(cpp_int A, cpp_int* witnesses_list, cpp_int* x_list, int x_list_len, int* nonce_list, int nonce_list_len, cpp_int n){
+	cpp_int* primes = new cpp_int[x_list_len];
+	for(int i=0;i<x_list_len;i++)
+		primes[i]=hash_to_prime(x_list[i], ACCUMULATED_PRIME_SIZE, nonce_list[i])[0];
+	
+	// printArray<cpp_int>(primes);	
+	cpp_int agg_wit = witnesses_list[0];
+	cpp_int product = primes[0];
+	// std::cout << agg_wit << std::endl;
+	// std::cout << product << std::endl;
+	for(int i=1;i<x_list_len;i++){
+		agg_wit = shamir_trick<cpp_int>(agg_wit,witnesses_list[i], product, primes[i], n);
+		product *= primes[i];
+	}
+	cpp_int* result = new cpp_int[3];
+	cpp_int* pe_result = prove_exponentiation(agg_wit, product, A, n);
+	result[0] = agg_wit;
+	result[1] = pe_result[0];
+	result[2] = pe_result[1];
+	return result; 
+}
 
+cpp_int* batch_delete_using_membership_proofs(cpp_int A_pre_delete, HashMapTable S, cpp_int* x_list, int x_list_len, cpp_int* proofs_list, int proofs_list_len, cpp_int n, int** agg_indexes, int agg_indexes_len){
+	bool is_aggregated = agg_indexes_len > 0;
+	if(is_aggregated && (proofs_list_len != agg_indexes_len))
+		return NULL;
+	if(!is_aggregated && (x_list_len != proofs_list_len))
+		return NULL;
+		
+	cpp_int* members = new cpp_int[x_list_len];
+	int members_len = x_list_len;
+	cpp_int product;
+	if(is_aggregated){
+		// sanity - verify each and every proof individually
+		members = new cpp_int[agg_indexes_len];
+		members_len = agg_indexes_len;
+		int* indexes = new int[2];
+		for(int i=0;i<agg_indexes_len;i++){
+			indexes = agg_indexes[i];
+			cpp_int* current_x_list = subarr<cpp_int>(x_list, x_list_len, indexes[0], indexes[1]);
+			int current_x_list_len = indexes[1]-indexes[0];
+			int* current_nonce_list = new int[current_x_list_len];
+			for(int j=0;j<current_x_list_len;j++)
+				current_nonce_list[j] = S.SearchKey(current_x_list[j]);
+
+			product = dunder_calculate_primes_product(current_x_list, current_x_list_len,  current_nonce_list, current_x_list_len);
+			members[i] = product;
+			for(int j=0;j<current_x_list_len;j++)
+				S.Remove(current_x_list[j]);
+		}
+	}
+				
+	else{
+		cpp_int x;
+		for(int i=0;i<x_list_len;i++){
+			x = x_list[i];
+			members[i] = hash_to_prime(x, ACCUMULATED_PRIME_SIZE, S.SearchKey(x))[0];
+			S.Remove(x);
+		}
+	}
+			
+	cpp_int A_post_delete = proofs_list[0];
+	product = members[0];
+	
+	for(int i=1;i<members_len;i++){
+		A_post_delete = shamir_trick<cpp_int>(A_post_delete, proofs_list[i], product, members[i], n);
+		product *= members[i];
+	}
+	cpp_int* result = new cpp_int[4];
+	cpp_int* pe_result = prove_exponentiation(A_post_delete, product, A_pre_delete, n);
+	result[0] = A_post_delete;
+	result[1] = product;
+	result[2] = pe_result[0];
+	result[3] = pe_result[1];
+	return result; 
+}
+
+void witness_update_after_batch_delete(cpp_int A_post_delete, cpp_int product, HashMapTable S, HashMapTable proofs, cpp_int n){
+	cpp_int* key = S.keys();
+	cpp_int x;
+	cpp_int hash_prime;
+	for(int i=0;i<S.length;i++){
+		x = key[i];
+		hash_prime = hash_to_prime(x, ACCUMULATED_PRIME_SIZE, S.SearchKey(x))[0];
+		proofs.Insert(x, int(shamir_trick<cpp_int>(A_post_delete, cpp_int(proofs.SearchKey(x)), product, x, n)));
+	}					
+}
 
 int main(){
 	int* x = new int[6];
